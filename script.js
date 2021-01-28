b9abe9de8e25e6b07465e99b35d72827cc1ff6ec
Villum <62429260+m-v-j@users.noreply.github.com>
2021-01-28 09:22:38 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var learnbutton = document.querySelector("#learnbtn");
+var climberbutton = document.querySelector("#firstclimberbtn");
+
+learnbutton.addEventListener("click", () => {
+  window.open("https://en.wikipedia.org/wiki/Mount_Everest");
+});
+
+climberbutton.addEventListener("click", () => {
+  window.open("https://www.thoughtco.com/the-first-to-climb-mount-everest-1779350");
+});
