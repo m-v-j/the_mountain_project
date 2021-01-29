0c3e3e7954e74295a82928f1831cb0d4a6451aa7
Villum <62429260+m-v-j@users.noreply.github.com>
2021-01-29 14:17:09 +0100

    script.js

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-var learnbutton = document.querySelector("#learnbtn");
-var climberbutton = document.querySelector("#firstclimberbtn");
+var learnButton = document.querySelector(".learnbtn");
+var climberButton = document.querySelector(".firstclimberbtn");
+const collapsedClass = "nav--collapsed";
+const lsKey = "nav--collapsed";
+const nav = document.querySelector(".nav");
+const navBorder = document.querySelector(".nav__border");
 
-learnbutton.addEventListener("click", () => {
+learnButton.addEventListener("click", () => {
   window.open("https://en.wikipedia.org/wiki/Mount_Everest");
 });
 
-climberbutton.addEventListener("click", () => {
-  window.open("https://www.thoughtco.com/the-first-to-climb-mount-everest-1779350");
+climberButton.addEventListener("click", () => {
+  window.open(
+    "https://www.thoughtco.com/the-first-to-climb-mount-everest-1779350"
+  );
+});
+
+if (localStorage.getItem(lsKey) === "true") {
+  nav.classList.add(collapsedClass);
+}
+
+navBorder.addEventListener("click", () => {
+  nav.classList.toggle(collapsedClass);
+  localStorage.setItem(lsKey, nav.classList.contains(collapsedClass));
 });
